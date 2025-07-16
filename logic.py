51e5465723d3f7655a4fd8a76574814426758736
Retkatmun <retkatmun1@gmail.com>
2025-07-16 12:03:13 +0000

    basic operation calculator

---
+++ logic.py
@@ -0,0 +1,65 @@
+#logical operator
+#or
+#operand
+#operator
+'''
+first_number = 4
+second_number = 6
+if first_number or second_number;
+      print("valid operation")
+'''
+#arithmetic operation
+#plus +
+#mul *
+#sub -
+
+#basic calculation appp
+
+print('''
+1. Addition
+2. Subtraction
+3. Multiplication
+4. Division
+''')
+
+'''
+print('++++++++++++++++')
+print('Enter two numbers to add')
+first_number = input('Enter first number: ')
+second_number = input('Enter second Number: ')
+sum = float(first_number) + float(second_number)
+print(f'{first_number} + {second_number} = {sum:.2f}')
+
+
+print('-------------')
+print('Enter two numbers to add')
+first_number = input('Enter first number: ')
+second_number = input('Enter second Number: ')
+sum = float(first_number) - float(second_number)
+print(f'{first_number} - {second_number} = {sum:.2f}')
+
+
+print('**************')
+print('Enter two numbers to add')
+first_number = input('Enter first number: ')
+second_number = input('Enter second Number: ')
+sum = float(first_number) * float(second_number)
+print(f'{first_number} * {second_number} = {sum:.2f}')
+
+
+print('////////////')
+print('Enter two numbers to add')
+first_number = input('Enter first number: ')
+second_number = input('Enter second Number: ')
+sum = float(first_number) / float(second_number)
+print(f'{first_number} / {second_number} = {sum:.2f}')
+
+'''
+print('Exponential')
+print('Enter two numbers to add')
+first_number = input('Enter first number: ')
+second_number = input('Enter second Number: ')
+sum = float(first_number) ** float(second_number)
+print(f'{first_number} ** {second_number} = {sum:.2f}')
+
+
